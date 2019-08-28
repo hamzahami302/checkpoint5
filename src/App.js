eecc7182072ad66cee66fad66250a077aa1b5181
hamza hami <hamihamza423@gmail.com>
2019-08-28 16:43:43 +0100

    commit 5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Img from './logo.svg';
 import './App.css';
+import Photo from './photo';
+import Name from './Name';
+import Mybutton from './Mybutton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Photo src={Img} />
+      <Name first="" last=""/>
+      <Mybutton>MyFacebook</Mybutton>
+      <Mybutton>LinkedIn</Mybutton>
+
+     
+         </div>
   );
 }
 
